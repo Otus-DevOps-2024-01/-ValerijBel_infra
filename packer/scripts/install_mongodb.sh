10eefab4618fe47679e30de02a111e3976c10322
Valerii Belotserkovnikov <valerij.bel@gmail.com>
2024-03-20 09:48:28 +0300

    hw6

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add - # После пайпа sudo не убрано - а то не отработает добавление ключа
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sleep 20
 apt-get update
 sleep 10
 apt-get install -y mongodb-org
